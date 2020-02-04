350a3ec9d7d37e99c21e3f1c2093e17927785c63
Nancy Alter <nancy.alter@gmail.com>
2020-02-03 22:49:03 -0500

    added new logic
    new logic enforces 1 manager requirement.  now the app allows the user to add only 1 manager and unlimited engineers and interns

--- app.js
+++ app.js
@@ -9,28 +9,52 @@ var engineerHTML = require("./templates/engineerhtml");
 var beginHTML = require("./templates/beginhtml");
 var endHTML = require("./templates/endhtml");
 var fs = require("fs");
+const path = "./output/myteam.html"
+
+var questManager = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the manager's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the manager's id?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is team manager's email address?"
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?"
+    }
+];
 
 var questMain = [
     {
         type: "input",
         name: "name",
-        message: "What is the team member name?"
+        message: "What is the team member's name?"
     },
     {
         type: "input",
         name: "id",
-        message: "What is the team member id?"
+        message: "What is the team member's id?"
     },
     {
         type: "input",
         name: "email",
-        message: "What is team member email address?"
+        message: "What is team member's email address?"
     },
     {
         type: 'list',
         message: 'What is the role of the team member?',
         name: 'role',
-        choices: ['Manager', 'Engineer', 'Intern']
+        choices: ['Engineer', 'Intern']
     }
 ];
 var questAsk =
@@ -47,12 +71,7 @@ var questInt = [
         name: "school",
         message: "What is the team member's school?"
     }];
-var questManager = [
-    {
-        type: "input",
-        name: "officeNumber",
-        message: "What is the team member's office number?"
-    }];
+
 var questEng = [
     {
         type: "input",
@@ -67,7 +86,7 @@ var output = [];
     function ask() {
         inquirer.prompt(questAsk).then(answers => {
           if (answers.askAgain) {
-            main();
+            mainQuestions();
           } else {
               console.log("The End");
               endWrite();
@@ -78,17 +97,31 @@ beginWrite();
 main();
 
 function main() {
-    console.log('Enter One Manager and up to 4 other Team Members');
-    mainQuestions();
-  }
+    enterManager();
+}
+function enterManager() {
+    console.log('Enter one manager to begin building your team. Then enter any number of engineers and interns.');
+    inquirer.prompt(questManager).then(answers => {
+        var a = answers;
+        const e = new Manager(a.name, a.id, a.email, answers.officeNumber);
+        fs.appendFile(path, managerHTML(e), 'UTF-8',
+            function (err) {
+                if (err) {
+                    throw err;
+                }
+            });
+        mainQuestions();
+    });
+}
 
 function mainQuestions() {
+    console.log('Enter team members');
     inquirer.prompt(questMain).then(answers => {
         moreQuestions(answers);
     });
 }
 function beginWrite() {
-    fs.writeFile("./output/myteam.html", beginHTML(),
+    fs.writeFile(path, beginHTML(),
     function (err) {
       if (err) {
         throw err;
@@ -106,22 +139,8 @@ function endWrite() {
 }
 
 function moreQuestions(answers) {
-    var path = "./output/myteam.html"
-    var a = answers;
-        if (answers.role === "Manager") {
-            inquirer.prompt(questManager).then(answers => {
-                const e = new Manager(a.name, a.id, a.email, answers.officeNumber);
-                fs.appendFile(path, managerHTML(e), 'UTF-8',
-                function (err) {
-                    if (err) {
-                      throw err;
-                    }
-                });
-                output.push(e);
-                ask();
-            });
-              
-        } else if (answers.role === "Engineer") {
+    var a = answers;              
+       if (answers.role === "Engineer") {
             inquirer.prompt(questEng).then(answers => {
                 const e = new Engineer(a.name, a.id, a.email, answers.github);
                 fs.appendFile(path, engineerHTML(e), 'UTF-8',
